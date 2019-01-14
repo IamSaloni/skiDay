88f21accad1e076bf3f25a7296fed68267724f06
Karun <karundeveloper@gmail.com>
2019-01-14 16:50:08 +0530

    passing props using stateless functional component

--- src/Components/SkiDayCount.js
+++ src/Components/SkiDayCount.js
@@ -1,36 +1,35 @@
-import  React, { Component} from 'react'
+import React, { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-class SkiDayCount extends Component {
-    percentToDecimal(decimal){
-      return ((decimal * 100) + '%')
-    }
-
-    calcGoalProgress (total, goal){
-        return this.percentToDecimal(total/goal) ///50z%
-    }
-    render() {
-        return (
-            <div className="ski-day-count">
-                <div className="total-days">
-                    <span>{this.props.total}</span>
-                    <span>days</span>
-                </div>
-                <div className="powder-days">
-                    <span>{this.props.powder}</span>
-                    <span>days</span>
-                </div>
-                <div className="backcountry-days">
-                    <span>{this.props.backcountry}</span>
-                    <span>days</span>
-                </div>
-                <div>
-                    <span>{this.calcGoalProgress(this.props.total, this.props.goal)}</span>
-                </div>
-
-            </div>
-        );
-    }
+const percentToDecimal = (decimal) => {
+    return ((decimal * 100) + '%')
 }
+
+const calcGoalProgress = (total, goal) => {
+    return percentToDecimal(total / goal) ///50z%
+}
+
+const SkiDayCount = (props) => (
+    <div className="ski-day-count">
+        <div className="total-days">
+            <span>{props.total}</span>
+            <span>days</span>
+        </div>
+        <div className="powder-days">
+            <span>{props.powder}</span>
+            <span>days</span>
+        </div>
+        <div className="backcountry-days">
+            <span>{props.backcountry}</span>
+            <span>days</span>
+        </div>
+        <div>
+            <span>{calcGoalProgress(props.total, props.goal)}</span>
+        </div>
+
+    </div>
+);
+
+
 
 export default SkiDayCount
