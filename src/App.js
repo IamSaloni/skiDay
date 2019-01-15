57c677346d4057f65cc27e6294a36960aa31e7df
Saloni Sharma <sharmasaloni061@gmail.com>
2019-01-15 22:49:35 +0530

    passed dynamic props

--- src/App.js
+++ src/App.js
@@ -4,36 +4,72 @@ import React, {Fragment} from "react";
 // import StudentList from "./Components/StudentList"
 import SkiDayList from "./Components/SkiDayList"
 import SkiDayCount from "./Components/SkiDayCount"
+import { all } from "q";
 
 class App extends React.Component {
-      constructor(props) {
-            super(props);
-            this.state = {
-                  allSkiDays : [
-                        {
-                            resort: "Squaw Valley",
-                            date: new Date("2019-1-14"),
-                            powder:true,
-                            backcountry:false
-                        },
-                        {
-                            resort: "Kirkwood",
-                            date: new Date("2019-1-15"),
-                            powder:false,
-                            backcountry:false
-                        },
-                        {
-                            resort: "Mt. Tallac",
-                            date: new Date("2019-1-18"),
-                            powder:false,
-                            backcountry:true
-                        },
-                    ]
-            }
-      }
-      render() {
-            return (
-                  // <StudentList
+  constructor(props) {
+    super(props);
+    this.state = {
+      allSkiDays: [
+        {
+          resort: "Squaw Valley",
+          date: new Date("2019-1-14"),
+          powder: true,
+          backcountry: false
+        },
+        {
+          resort: "Kirkwood",
+          date: new Date("2019-1-15"),
+          powder: false,
+          backcountry: true
+        },
+        {
+          resort: "Mt. Tallac",
+          date: new Date("2019-1-18"),
+          powder: false,
+          backcountry: true
+        }
+      ]
+    };
+    this.countDays = this.countDays.bind(this); 
+  }
+
+  countDays(cond) {
+    // cond = "powder", "backcountry", undefined
+    if(cond) {
+      return this.state.allSkiDays.filter((skiDay, index) => {
+            //return skiday.powder, siDay.backcountry // we didnt use this to use the cond variable passed in function
+            return skiDay[cond];
+          }).length;
+    }
+    else {
+          return this.state.allSkiDays.length
+    }
+  }
+
+
+  render() {
+    return (
+      <Fragment>
+        <SkiDayList days={this.state.allSkiDays} />
+        <SkiDayCount
+          total={this.countDays()}
+          powder={this.countDays("powder")}
+          backcountry={this.countDays("backcountry")}
+          goal={100}
+        />
+      </Fragment>
+    );
+  }
+}
+
+
+
+export default App;
+
+
+
+ // <StudentList
                   //       students={[
                   //             {
                   //                   "name": "Hall Parker",
@@ -51,16 +87,8 @@ class App extends React.Component {
                   //             }]
                   //       }
                   // />
-                  <Fragment>
-                  <SkiDayList days={
-                        this.state.allSkiDays
-                    } />
-                  <SkiDayCount />
-                  </Fragment>
-            )
-      }
-}
 
 
-
-export default App;
+                  //allskiDays 
+                  // if that skiday is powder day 
+                  //
