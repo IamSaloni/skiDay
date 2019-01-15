2b1a7a40a3a3c6f345df7bd3f2b12eed0c897024
Karun <karundeveloper@gmail.com>
2019-01-15 13:47:11 +0530

    studentList Created

--- src/App.js
+++ src/App.js
@@ -5,74 +5,75 @@
 // 2, Saloni, 7th
 // 3, Arun, Btech
 import React from "react";
-class StudentList extends React.Component {
-  render() {
-    return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>Student Name</th>
-              <th>Roll No</th>
-              <th>Standard</th>
-            </tr>
-          </thead>
-        <tbody>
-              {this.props.students.map((student, i)=>{
-                    return <tr key={i}>
-                          <td >{student.name}</td>
-                          <td >{student.id}</td>
-                          <td >{student.Standard}</td>
 
-                    </tr>
-              })}
-        </tbody>
-        </table>
-      </div>
-    );
-  }
+
+
+const Student = ({name,  id, standard}) => (
+      <tr>
+
+            <td>{name}</td>
+            <td>{id}</td>
+            <td>{standard}</td>
+      </tr>
+
+)
+
+
+
+class StudentList extends React.Component {
+      render() {
+            return (
+                  <div>
+                        <table>
+                              <thead>
+                                    <tr>
+                                          <th>Student Name</th>
+                                          <th>Roll No</th>
+                                          <th>Standard</th>
+                                    </tr>
+                              </thead>
+                              <tbody>
+                                    {this.props.students.map((student, i) => {
+                                          return <Student key={i}
+                                                            name={student.name}
+                                                            id={student.id}
+                                                            standard={student.standard} />
+                                    })}
+                              </tbody>
+                        </table>
+                  </div>
+            );
+      }
 }
+
 
 class App extends React.Component {
-  render() {
-    return (
-      <StudentList
-        students={[
-          {
-            name: "Hall Parker",
-            id: 1,
-            Standard: "10th"
-          },
-          {
-            name: "Terrie Underwood",
-            id: 2,
-            Standard: "10th"
-          },
-          {
-            name: "Briana Britt",
-            id: 3,
-            Standard: "11th"
-          },
-          {
-            name: "Lara Cardenas",
-            id: 4,
-            Standard: "11th"
-          },
-          {
-            name: "Wilder Ware",
-            id: 5,
-            Standard: "11th"
-          }
-        ]}
-      />
-    );
-  }
+
+      componentDidMount() {
+            
+      }
+      render() {
+            return (
+                  <StudentList
+                        students={[
+                              {
+                                    "name": "Hall Parker",
+                                    "id": 1,
+                                    "standard": "10th"
+                              },
+                              {
+                                    "name": "Terrie Underwood",
+                                    "id": 2,
+                                    "standard": "10th"
+                              }, {
+                                    "name": "Hall Parker",
+                                    "id": 4,
+                                    "standard": "10th"
+                              }]
+                        }
+                  />
+            );
+      }
 }
-
-// import React from 'react'
-
-// const App = () => (<h1 id={'title'}>
-//       Hello world
-//    </h1>)
 
 export default App;
